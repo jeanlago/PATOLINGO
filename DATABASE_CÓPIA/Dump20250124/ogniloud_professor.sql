bea18818f7fc8a6759e435e7f51414a0960c9fc3
Jean <100036946+jeanlago@users.noreply.github.com>
2025-01-28 23:23:20 -0300

    Fix: - Correção em perguntas que possuiam respostas repetidas. - Correção em perguntas que começavam com letras minúsculas no banco de dados.

--- DATABASE_CÓPIA/Dump20250124/ogniloud_professor.sql
+++ DATABASE_CÓPIA/Dump20250124/ogniloud_professor.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `ogniloud` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `ogniloud`;
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ogniloud
 -- ------------------------------------------------------
--- Server version	8.0.41
+-- Server version	8.0.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-28 22:45:35
+-- Dump completed on 2025-01-28 23:20:15
--- DATABASE_CÓPIA/Dump20250124/ogniloud_progresso_alunos.sql
+++ DATABASE_CÓPIA/Dump20250124/ogniloud_progresso_alunos.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `ogniloud` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `ogniloud`;
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ogniloud
 -- ------------------------------------------------------
--- Server version	8.0.41
+-- Server version	8.0.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,7 +41,7 @@ CREATE TABLE `progresso_alunos` (
 
 LOCK TABLES `progresso_alunos` WRITE;
 /*!40000 ALTER TABLE `progresso_alunos` DISABLE KEYS */;
-INSERT INTO `progresso_alunos` VALUES (1,2,50,1),(2,4,80,1),(3,7,4,1),(4,8,10,1),(5,9,0,1);
+INSERT INTO `progresso_alunos` VALUES (1,2,50,1),(2,4,90,1),(3,7,4,1),(4,8,10,1),(5,9,0,1);
 /*!40000 ALTER TABLE `progresso_alunos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-28 22:45:35
+-- Dump completed on 2025-01-28 23:20:14
